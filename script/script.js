d4cf6363f63c8cde469a6ea0ce0b4f7327df4b6f
Sitmonternna <112128532+Sitmonternna@users.noreply.github.com>
2024-04-13 23:21:25 -0400

    Update script.js

--- script/script.js
+++ script/script.js
@@ -2,26 +2,7 @@
 gsap.set(".introScroll", { x:0, y:0, opacity:1, duration: 1, repeat: -1, yoyo: true, ease: "linear"});
 gsap.to(".introScroll", {x:0, y:30, opacity: 0, duration: 1, repeat: -1, yoyo: true, ease: "linear"});
 
-
-// Animation de la sprite sheet
-gsap.to(".animationpersonnage", {
-    duration: 10,
-    backgroundPosition: "-32768px",
-    repeat: -1,
-    ease: "steps(20)",
-});
-
-// Animation de la sprite sheet
-gsap.to(".animationVillain", {
-    duration: 8,
-    backgroundPositionX: "-237600px",
-    repeat: -1,
-    ease: "steps(10)",
-});
-
-
 // Écouteur d'événement scroll
-
 let scrollingTimer; 
 
 window.addEventListener('scroll', function() {
@@ -29,7 +10,7 @@ window.addEventListener('scroll', function() {
 
     clearTimeout(scrollingTimer); // Annule le minuteur s'il existe déjà
     scrollingTimer = setTimeout(function() {
-        document.body.classList.remove('is-scrolling'); // Enlève la classe .is-scrolling après 100ms
+        document.body.classList.remove('is-scrolling'); // Enlève la classe .is-scrolling après 2000ms
     }, 2000);
 });
 
